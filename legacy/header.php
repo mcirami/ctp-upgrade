f622b4291ce1c7a45021441dfa91ca7cc8534b75
Matteo Cirami <mcirami@users.noreply.github.com>
2024-02-02 09:15:46 -0600

    changed main js file version

--- legacy/header.php
+++ legacy/header.php
@@ -58,7 +58,7 @@ $navBar = new \LeadMax\TrackYourStats\System\NavBar(\LeadMax\TrackYourStats\Syst
 
     <script type="text/javascript" src="<?php echo $webroot; ?>js/jscolor.min.js"></script>
     <link rel="stylesheet" href="css/jquery-ui.min.css"/>
-    <script type="text/javascript" src="<?php echo $webroot; ?>js/main.js?v=2.2"></script>
+    <script type="text/javascript" src="<?php echo $webroot; ?>js/main.js?v=2.3"></script>
     <script type="text/javascript" src="<?php echo $webroot; ?>js/drawer.js"></script>
     <script type="text/javascript" src="<?php echo $webroot; ?>js/iscroll.min.js"></script>
 
