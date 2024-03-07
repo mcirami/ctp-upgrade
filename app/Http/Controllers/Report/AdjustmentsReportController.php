07485e471532fbc5b43fa941ad7957197bddbc8d
Matteo Cirami <mcirami@users.noreply.github.com>
2024-03-07 09:46:21 -0600

    removed test code for updating max cap

--- app/Http/Controllers/Report/AdjustmentsReportController.php
+++ app/Http/Controllers/Report/AdjustmentsReportController.php
@@ -29,26 +29,6 @@ class AdjustmentsReportController extends ReportController
         $reporter = new Reporter($repo);
         $reporter->addFilter(new DollarSign(['paid']));
 
-	    $tz = 'America/New_York';
-	    $dateToday = \Illuminate\Support\Carbon::today($tz)->add(1, 'day')->format('Y-m-d H:i:s');
-	    $carbonToday = Carbon::createFromFormat('Y-m-d H:i:s', $dateToday, $tz);
-	    $dateNow = $carbonToday->setTimezone("UTC");
-
-	    $clicksLog = new Logger('clicks');
-	    $clicksLog->pushHandler(new StreamHandler(storage_path('logs/clicks.log')), Logger::INFO);
-	    $log = [
-		    'now'           => $dateNow,
-		    'offerID'       => 905
-	    ];
-	    $clicksLog->info('Click', $log);
-
-	    if($dateNow > '2024-03-08 04:59:59') {
-		    $db   = \LeadMax\TrackYourStats\Database\DatabaseConnection::getInstance();
-		    $sql  = "UPDATE offer_caps SET max_cap_status = 0, max_cap_date = NULL WHERE offer_idoffer = 905";
-		    $prep = $db->prepare( $sql );
-		    $prep->execute();
-	    }
-
         return view('report.adjustments', compact('reporter','dates'));
     }
 
