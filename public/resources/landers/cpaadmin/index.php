54dba13a83d25fda66ac942469619ca9deb12df1
Matteo Cirami <mcirami@users.noreply.github.com>
2024-10-09 09:40:05 -0500

    changed CPA Admin lander

--- public/resources/landers/cpaadmin/index.php
+++ public/resources/landers/cpaadmin/index.php
@@ -325,12 +325,6 @@
 			</div>
 			<div class="bottom_content">
 				<p>We have a solution for every traffic source!</p>
-				<a class="button blue with_arrow" href="/contact-us">
-					Contact Us
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-						<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-					</svg>
-				</a>
 			</div>
 		</div>
 	</section>
@@ -349,9 +343,6 @@
 				</li>
 				<li>
 					<a href="https://cpaadmin.com/login.php">Sign In</a>
-				</li>
-				<li>
-					<a href="/contact-us">Contact Us</a>
 				</li>
 			</ul>
 			<p class="copy">&copy; CPA Admin | All rights reserved.</p>
