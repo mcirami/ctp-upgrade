2bd1a4c26b8f1e3d1c706026e5deeb02555ffa5f
Matteo Cirami <mcirami@users.noreply.github.com>
2024-10-09 09:30:10 -0500

    changed CPA Admin lander

--- public/resources/landers/cpaadmin/index.php
+++ public/resources/landers/cpaadmin/index.php
@@ -67,20 +67,20 @@
 					<h2>Private Network for<br /><span>Owners & Managers</span></h2>
 					<p>Our Exclusive Network has over 50 years<br />
 					   of combined experience in CPA marketing. </p>
-					<!-- <div class="buttons_wrap">
-						<a class="button blue with_arrow" href="/contact-us">
+					<div class="buttons_wrap">
+						<!-- <a class="button blue with_arrow" href="/contact-us">
 							Contact Us
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
 							</svg>
-						</a>
+						</a> -->
 						<a class="button green with_arrow" href="#how_it_works">
 							Find Out More
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
 								<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
 							</svg>
 						</a>
-					</div> -->
+					</div>
 				</article>
 				<div class="grid_wrap">
 					<article>
