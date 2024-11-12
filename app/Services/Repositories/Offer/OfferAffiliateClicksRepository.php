85f3d150d8ed93f829b2e0fd520cc524021c04cb
Matteo Cirami <mcirami@users.noreply.github.com>
2024-11-12 10:20:18 -0600

    added new query for all types of  users conversions

--- app/Services/Repositories/Offer/OfferAffiliateClicksRepository.php
+++ app/Services/Repositories/Offer/OfferAffiliateClicksRepository.php
@@ -89,125 +89,56 @@ class OfferAffiliateClicksRepository implements Repository
     }
 
     public function getOfferConversionsForAgent($start, $end) {
-        return \DB::query()->select([
-            'rep.idrep as user_id',
-            'rep.user_name',
-            'offer.idoffer as offer_id',
-            'offer.offer_name',
-            \DB::raw('COUNT(clks.idclicks) as clicks'),
-            \DB::raw('COUNT(cnvs.click_id) as conversions')
-        ])->from('rep_has_offer as rho')
-            ->join('rep', function ($jc) {
-                /* @var $jc JoinClause */
-                $jc->on('rep.idrep', 'rho.rep_idrep');
-               /* $jc->where('rep.lft', '>', $this->user->lft);
-                $jc->where('rep.rgt', '<', $this->user->rgt);*/
-            })
-            ->join('offer', 'rho.offer_idoffer', 'offer.idoffer')
-            ->join('clicks as clks', function ($jc) use ($start, $end) {
-                /* @var $q JoinClause */
-                $jc->on('clks.rep_idrep', 'rho.rep_idrep')
-                    ->on('clks.offer_idoffer', 'rho.offer_idoffer')
-                    ->whereBetween('clks.first_timestamp', [$start, $end]);
-            })
-            ->leftJoin('conversions as cnvs', 'cnvs.click_id', 'clks.idclicks')
-            ->where('offer.idoffer', $this->offerId)
-            ->where('rho.rep_idrep', $this->user->idrep)
-            ->groupBy('rep.user_name', 'rep.idrep', 'offer_id')
-            ->orderBy('conversions', 'DESC');
+        return DB::table('clicks')
+        ->where('offer_idoffer', '=', $this->offerId)
+        ->where('rep_idrep', '=', $this->user->idrep)
+        ->whereBetween('first_timestamp',[$start, $end])
+        ->join('rep', 'idrep', '=', 'clicks.rep_idrep')
+        ->rightJoin('conversions', 'conversions.click_id', '=', 'clicks.idclicks')
+        ->leftJoin('offer', 'offer.idoffer', '=', 'clicks.offer_idoffer')
+        ->select('rep.idrep as user_id', 'rep.user_name', 'offer.idoffer as offer_id', 'offer.offer_name', DB::raw('COUNT(clicks.idclicks) as clicks'),
+        DB::raw('COUNT(conversions.click_id) as conversions'))->groupBy('rep.user_name', 'rep.idrep', 'offer_id')->orderBy('conversions', 'DESC');
     }
 
     public function getOfferConversionsForManager($start, $end) {
-        return \DB::query()->select([
-            'rep.idrep as user_id',
-            'rep.user_name',
-            'offer.idoffer as offer_id',
-            'offer.offer_name',
-            \DB::raw('COUNT(clks.idclicks) as clicks'),
-            \DB::raw('COUNT(cnvs.click_id) as conversions')
-        ])->from('rep_has_offer as rho')
-            ->join('rep', function ($jc) {
-                /* @var $jc JoinClause */
-                $jc->on('rep.idrep', 'rho.rep_idrep')->where('rep.referrer_repid', $this->user->idrep);
-               /* $jc->where('rep.lft', '>', $this->user->lft);
-                $jc->where('rep.rgt', '<', $this->user->rgt);*/
-            })
-            ->join('offer', 'rho.offer_idoffer', 'offer.idoffer')
-            ->join('clicks as clks', function ($jc) use ($start, $end) {
-                /* @var $q JoinClause */
-                $jc->on('clks.rep_idrep', 'rho.rep_idrep')
-                    ->on('clks.offer_idoffer', 'rho.offer_idoffer')
-                    ->whereBetween('clks.first_timestamp', [$start, $end]);
-            })
-            ->leftJoin('conversions as cnvs', 'cnvs.click_id', 'clks.idclicks')
-            ->where('offer.idoffer', $this->offerId)
-            ->groupBy('rep.user_name', 'rep.idrep', 'offer_id')
-            ->orderBy('conversions', 'DESC');
+
+        return DB::table('clicks')
+        ->where('offer_idoffer', '=', $this->offerId)
+        ->whereBetween('first_timestamp',[$start, $end])
+        ->join('rep', function($query) {
+            $query->on('idrep', '=', 'clicks.rep_idrep')
+            ->where('rep.referrer_repid', $this->user->idrep);
+        })
+        ->rightJoin('conversions', 'conversions.click_id', '=', 'clicks.idclicks')
+        ->leftJoin('offer', 'offer.idoffer', '=', 'clicks.offer_idoffer')
+        ->select('rep.idrep as user_id', 'rep.user_name', 'offer.idoffer as offer_id', 'offer.offer_name', DB::raw('COUNT(clicks.idclicks) as clicks'),
+        DB::raw('COUNT(conversions.click_id) as conversions'))->groupBy('rep.user_name', 'rep.idrep', 'offer_id')->orderBy('conversions', 'DESC');
     }
 
     public function getOfferConversionsForAdmin($start, $end) {
 
         $managers = User::where('referrer_repid', $this->user->idrep)->pluck('idrep')->toArray();
 
-        return \DB::query()->select([
-            'rep.idrep as user_id',
-            'rep.user_name',
-            'offer.idoffer as offer_id',
-            'offer.offer_name',
-            \DB::raw('COUNT(clks.idclicks) as clicks'),
-            \DB::raw('COUNT(cnvs.click_id) as conversions')
-        ])->from('rep_has_offer as rho')
-            ->join('rep', function ($jc) use($managers)  {
-                /* @var $jc JoinClause */
-                $jc->on('rep.idrep', 'rho.rep_idrep')->whereIn('rep.referrer_repid', $managers);
-               /* $jc->where('rep.lft', '>', $this->user->lft);
-                $jc->where('rep.rgt', '<', $this->user->rgt);*/
-            })
-            ->join('offer', 'rho.offer_idoffer', 'offer.idoffer')
-            ->join('clicks as clks', function ($jc) use ($start, $end) {
-                /* @var $q JoinClause */
-                $jc->on('clks.rep_idrep', 'rho.rep_idrep')
-                    ->on('clks.offer_idoffer', 'rho.offer_idoffer')
-                    ->whereBetween('clks.first_timestamp', [$start, $end]);
-            })
-            ->leftJoin('conversions as cnvs', 'cnvs.click_id', 'clks.idclicks')
-            ->where('offer.idoffer', $this->offerId)
-            ->groupBy('rep.user_name', 'rep.idrep', 'offer_id')
-            ->orderBy('conversions', 'DESC');
+        return DB::table('clicks')
+        ->where('offer_idoffer', '=', $this->offerId)
+        ->whereBetween('first_timestamp',[$start, $end])
+        ->join('rep', function($query) use($managers) {
+            $query->on('idrep', '=', 'clicks.rep_idrep')
+            ->whereIn('rep.referrer_repid', $managers);
+        })
+        ->rightJoin('conversions', 'conversions.click_id', '=', 'clicks.idclicks')
+        ->leftJoin('offer', 'offer.idoffer', '=', 'clicks.offer_idoffer')
+        ->select('rep.idrep as user_id', 'rep.user_name', 'offer.idoffer as offer_id', 'offer.offer_name', DB::raw('COUNT(clicks.idclicks) as clicks'),
+        DB::raw('COUNT(conversions.click_id) as conversions'))->groupBy('rep.user_name', 'rep.idrep', 'offer_id')->orderBy('conversions', 'DESC');
     }
 
     public function getOfferConversionsForGod($start, $end) {
-       /*  return \DB::query()->select([
-            'rep.idrep as user_id',
-            'rep.user_name',
-            'offer.idoffer as offer_id',
-            'offer.offer_name',
-            \DB::raw('COUNT(clks.idclicks) as clicks'),
-            \DB::raw('COUNT(cnvs.click_id) as conversions')
-        ])->from('rep_has_offer as rho')
-            ->join('rep', function ($jc) { */
-                /* @var $jc JoinClause */
-               /*  $jc->on('rep.idrep', 'rho.rep_idrep'); */
-               /* $jc->where('rep.lft', '>', $this->user->lft);
-                $jc->where('rep.rgt', '<', $this->user->rgt);*/
-           /*  })
-            ->join('offer', 'rho.offer_idoffer', 'offer.idoffer')
-            ->join('clicks as clks', function ($jc) use ($start, $end) { */
-                /* @var $q JoinClause */
-               /*  $jc->on('clks.rep_idrep', 'rho.rep_idrep')
-                    ->on('clks.offer_idoffer', 'rho.offer_idoffer')
-                    ->whereBetween('clks.first_timestamp', [$start, $end]);
-            })
-            ->leftJoin('conversions as cnvs', 'cnvs.click_id', 'clks.idclicks')
-            ->where('offer.idoffer', $this->offerId)
-            ->groupBy('rep.user_name', 'rep.idrep', 'offer_id')
-            ->orderBy('conversions', 'DESC'); */
 
         return DB::table('clicks')
         ->where('offer_idoffer', '=', $this->offerId)
         ->whereBetween('first_timestamp',[$start, $end])
-        ->leftJoin('rep', 'idrep', '=', 'clicks.rep_idrep')
-        ->leftJoin('conversions', 'conversions.click_id', '=', 'clicks.idclicks')
+        ->join('rep', 'idrep', '=', 'clicks.rep_idrep')
+        ->rightJoin('conversions', 'conversions.click_id', '=', 'clicks.idclicks')
         ->leftJoin('offer', 'offer.idoffer', '=', 'clicks.offer_idoffer')
         ->select('rep.idrep as user_id', 'rep.user_name', 'offer.idoffer as offer_id', 'offer.offer_name', DB::raw('COUNT(clicks.idclicks) as clicks'),
         DB::raw('COUNT(conversions.click_id) as conversions'))->groupBy('rep.user_name', 'rep.idrep', 'offer_id')->orderBy('conversions', 'DESC');
