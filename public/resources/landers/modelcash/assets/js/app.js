b24510050980737fb4e083e03db27593ef647aaf
Matteo Cirami <mcirami@users.noreply.github.com>
2025-02-13 15:03:10 -0600

    adjusting styles and text for login and home page model cash

--- public/resources/landers/modelcash/assets/js/app.js
+++ public/resources/landers/modelcash/assets/js/app.js
@@ -1,45 +1,42 @@
-/* window.$ = window.jquery = require("jquery");
-window.Popper = require("@popperjs/core");
-require("bootstrap"); */
 ("use strict");
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    let items = document.querySelectorAll(".carousel .carousel-item");
-    let minPerSlide = 0;
-
-    if (window.innerWidth < 768) {
-        minPerSlide = 6;
-    } else if (window.innerWidth < 992) {
-        minPerSlide = 8;
-    } else {
-        minPerSlide = 12;
-    }
-
-    setCarousel(items, minPerSlide);
-    function setCarousel(items, minPerSlide) {
-        items.forEach((el) => {
-            let next = el.nextElementSibling;
-            for (var i = 1; i < minPerSlide; i++) {
-                if (!next) {
-                    // wrap carousel by using first child
-                    next = items[0];
-                }
-                let cloneChild = next.cloneNode(true);
-                el.appendChild(cloneChild.children[0]);
-                next = next.nextElementSibling;
-            }
-        });
-    }
-
-    $(window).on("resize", function () {
-        if (window.innerWidth < 768) {
-            minPerSlide = 6;
-        } else if (window.innerWidth < 992) {
-            minPerSlide = 8;
-        } else {
-            minPerSlide = 12;
-        }
-
-        setCarousel(items, minPerSlide);
+    let splide = new Splide(".splide", {
+        type: "loop",
+        perPage: 14,
+        arrows: false,
+        pagination: false,
+        speed: 860,
+        autoScroll: {
+            speed: 0.5,
+            pauseOnHover: false,
+        },
+        lazyLoad: "nearby",
+        drag: "free",
+        focus: "center",
+        gap: "3em",
+        mediaQuery: "max",
+        breakpoints: {
+            992: {
+                perPage: 10,
+                autoScroll: {
+                    speed: 0.5,
+                },
+            },
+            767: {
+                perPage: 8,
+                autoScroll: {
+                    speed: 0.5,
+                },
+            },
+            550: {
+                perPage: 6,
+                autoScroll: {
+                    speed: 0.5,
+                },
+            },
+        },
     });
+
+    splide.mount(window.splide.Extensions);
 });
