1ac4e64277cfe87f5f077a6848997e0d61bd4aad
Matteo Cirami <mcirami@users.noreply.github.com>
2025-02-21 16:09:01 -0600

    adjusting some css

--- legacy/signup_success.php
+++ legacy/signup_success.php
@@ -22,7 +22,7 @@ $pending = (isset($_GET["pending"]) && $_GET["pending"] != "") ? $_GET["pending"
 	<link rel = "shortcut icon" type = "image/ico"
 		  href = "<?PHP echo \LeadMax\TrackYourStats\System\Company::loadFromSession()->getImgDir() . "/favicon.ico"; ?>"/>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<link rel = "stylesheet" type = "text/css" href = "<?php echo $webroot; ?>css/default.css?v=1.1"/>
+	<link rel = "stylesheet" type = "text/css" href = "<?php echo $webroot; ?>css/default.css?v=1.2"/>
 
 	<link rel = "stylesheet" media = "screen" type = "text/css"
 	      href = "<?php echo $webroot; ?>css/company.css"/>
--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -9,7 +9,7 @@
           href="<?php echo $webroot . "/" . \LeadMax\TrackYourStats\System\Company::loadFromSession()
                   ->getImgDir() . "/favicon.ico?v=2"; ?>"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="<?php echo $webroot; ?>css/default.css?v=3.1"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo $webroot; ?>css/default.css?v=3.2"/>
     <link rel="stylesheet" href="<?php echo $webroot; ?>css/company.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.min.js" integrity="sha256-AlTido85uXPlSyyaZNsjJXeCs07eSv3r43kyCVc8ChI=" crossorigin="anonymous"></script>
