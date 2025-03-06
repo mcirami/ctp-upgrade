0710638089c5bde16a8e00ccfc281d1be4bd435b
Matteo Cirami <mcirami@users.noreply.github.com>
2025-03-06 16:55:16 -0600

    debugging with my IP

--- src/Offer/Rules/Geo.php
+++ src/Offer/Rules/Geo.php
@@ -327,7 +327,7 @@ class Geo implements Rule
 	        }
 
             //trys to get their iso code and postal
-            $this->record = $this->geoReader->city('174.86.201.244');
+            $this->record = $this->geoReader->city($ip);
             $this->countryISO = $this->record->country->isoCode;
 
         } catch (\Exception $e) // if their ip wasn't in the db, set default values
