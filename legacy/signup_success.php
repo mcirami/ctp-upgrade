2bd361eef39e56930deb612320270d453a915d31
Matteo Cirami <mcirami@users.noreply.github.com>
2025-05-19 11:59:47 -0500

    removed contact info on signup success

--- legacy/signup_success.php
+++ legacy/signup_success.php
@@ -135,35 +135,9 @@ $pending = (isset($_GET["pending"]) && $_GET["pending"] != "") ? $_GET["pending"
 							</div>
 							<h3>Please contact us for approval</h3>
 							<div class="columns_wrap">
-								<div class="column">
-									<h4>Jeff:</h4>
-									<p>
-										<span>Skype:</span>
-										<a href="skype:moneylovers.jeff?add">moneylovers.jeff</a>
-									</p>
-									<p>
-										<span>Email: </span>
-										<a href="mailto:jeff@moneylovers.com">jeff@moneylovers.com</a></p>
-									<p>
-										<span>Telegram:</span>
-										<a href="https://t.me/jefftoch">jefftoch</a>
-									</p>
-								</div>
-								<div class="column">
-									<h4>Matteo</h4>
-									<p>
-										<span>Skype:</span>
-										<a href="skype:moneylovers.matteo?add">moneylovers.matteo</a>
-									</p>
-									<p>
-										<span>Email:</span>
-										<a href="mailto:matteo@moneylovers.com">matteo@moneylovers.com</a>
-									</p>
-									<p>
-										<span>Telegram:</span>
-										<a href="https://t.me/moneylovers_matteo">moneylovers_matteo</a>
-									</p>
-								</div>
+								<p>
+									Contact the manager who sent you your signup link with any questions.
+								</p>
 							</div>
 						</div>
 				<?php
