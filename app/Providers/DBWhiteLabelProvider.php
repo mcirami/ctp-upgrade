e19a70f4c7d1550e1d4afcc1f66c91e0b0560cf7
Matteo Cirami <mcirami@users.noreply.github.com>
2025-06-17 12:19:58 -0500

    removed db logging in database.php file causing errors when running composer

--- app/Providers/DBWhiteLabelProvider.php
+++ app/Providers/DBWhiteLabelProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Company;
-use App\Http\Controllers\DB;
 use App\Services\DBWhiteLabelService;
 use Illuminate\Support\ServiceProvider;
 
--- config/app.php
+++ config/app.php
@@ -194,7 +194,6 @@ return [
         'DB' => Illuminate\Support\Facades\DB::class,
         'Eloquent' => Illuminate\Database\Eloquent\Model::class,
         'Event' => Illuminate\Support\Facades\Event::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
@@ -216,7 +215,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
-
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'PDF' => Barryvdh\Snappy\Facades\SnappyPdf::class,
         'SnappyImage' => Barryvdh\Snappy\Facades\SnappyImage::class,
 
