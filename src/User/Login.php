8b6f1df395d025abc1e298f0f3bc0180f5fb4a4d
Matteo Cirami <mcirami@users.noreply.github.com>
2025-07-30 11:03:14 -0500

    adding check for IPv4 only

--- src/User/Login.php
+++ src/User/Login.php
@@ -85,10 +85,10 @@ class Login
 			        $stmt->execute();
 			        $whiteListIPs  = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
-					$clientIP = $this->getClientIPv4();
-			        Log::info("Login attempt from IP: " . $clientIP);
+					//$clientIP = $this->getClientIPv4();
+			        //Log::info("Login attempt from IP: " . $clientIP);
 			        if(Session::userType() == \App\Privilege::ROLE_GOD &&
-			           !in_array($clientIP, $whiteListIPs) && $_SERVER['REMOTE_ADDR'] != '127.0.0.1'
+			           !in_array($_SERVER["REMOTE_ADDR"], $whiteListIPs) && $_SERVER['REMOTE_ADDR'] != '127.0.0.1'
 			        ) {
 				        return self::RESULT_BANNED;
 			        }
