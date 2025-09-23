92df44860972e03cbf805da66933df1d12601e1a
Matteo Cirami <mcirami@users.noreply.github.com>
2025-09-23 14:41:58 -0500

    Add seeder for SUN referral accounts

---
+++ database/seeds/SunUsersSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SunUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach (range(1, 100) as $index) {
+            $userName = sprintf('SUN%02d', $index);
+
+            if (User::where('user_name', $userName)->exists()) {
+                continue;
+            }
+
+            $user = new User();
+            $user->user_name = $userName;
+            $user->email = strtolower($userName) . '@' . strtolower($userName) . '.com';
+            $user->password = Hash::make($userName . '!!!');
+            $user->status = 1;
+            $user->referrer_repid = 1012;
+            $user->rep_timestamp = Carbon::now();
+            $user->company_name = '';
+            $user->save();
+        }
+    }
+}
